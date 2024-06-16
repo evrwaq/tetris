0f611cd8f716f6d8b4e9f6d7d6f443efee5efdfa
João Victor Veidz <evrwaq@gmail.com>
2024-06-16 12:11:50 -0300

    feat: implement DrawEmptyCells

--- Assets/Scripts/Core/Board.cs
+++ Assets/Scripts/Core/Board.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-  // Start is called before the first frame update
-  void Start()
-  {
+  public Transform emptySprite;
+  public int height = 30;
+  public int width = 10;
 
+  private void Start()
+  {
+    this.DrawEmptyCells();
   }
 
-  // Update is called once per frame
-  void Update()
+  private void DrawEmptyCells()
   {
-
+    if (this.emptySprite)
+    {
+      for (int y = 0; y < this.height; y++)
+      {
+        for (int x = 0; x < this.width; x++)
+        {
+          Transform clone;
+          clone = Instantiate(this.emptySprite, new Vector3(x, y, 0), Quaternion.identity);
+          clone.name = "Board Space (x = " + x.ToString() + ", y = " + y.ToString() + ")";
+          clone.transform.parent = this.transform;
+        }
+      }
+    }
+    else
+    {
+      Debug.Log("WARNING! Please assign the emptySprite object");
+    }
   }
 }
